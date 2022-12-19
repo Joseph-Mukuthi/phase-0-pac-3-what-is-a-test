a0fc9818c7549aaa7557285de846ccdc90054a97
Joseph-Mukuthi <joseph.mukuthi@student.moringaschool.com>
2022-12-19 13:47:32 +0300

    Test

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,27 +1,27 @@
-require ( './root.js' );
+require("./root.js");
 
 const { name, height, message } = require("../index.js");
 
-/*
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
+
+describe("what-is-a-test", () => {
+  describe("Name", () => {
+    it("returns \"Susan\"", () => {
+      expect(name).toEqual("Susan")
     })
   })
 
 
-  describe('Height', () => {
-    it('is less than 40', () => {
+  describe("Height", () => {
+    it("is less than 40", () => {
       expect(height).toBeLessThan(40)
     })
   })
 
-  describe('Message', () => {
-    it('gives the name and height', () => {
+  describe("Message", () => {
+    it("gives the name and height", () => {
       expect(message).toInclude(name)
       expect(message).toInclude(height)
     })
   })
 })
-*/
+
